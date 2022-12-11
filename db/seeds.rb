23cbed8fb8000232111a33befdbbf7172cdfd934
Nahian Rifaat <nrifaat26@gmail.com>
2022-12-11 17:27:37 +0600

    "Depot- second commit with unit testing"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
+# encoding: utf-8
 Product.delete_all
 Product.create!(title: 'Docker for Rails Developers',
-                description: %(<p>
+                description:
+                  %{<p>
       <em>Build, Ship, and Run Your Applications Everywhere</em>
       Docker does for DevOps what Rails did for web development—it gives you
       a new set of superpowers. Gone are “works on my machine” woes and lengthy
@@ -19,10 +20,9 @@ Product.create!(title: 'Docker for Rails Developers',
       fundamental to software development. Go from zero all the way to production
       as Docker transforms the massive leap of deploying your app in the cloud
       into a baby step.
-      </p>),
+      </p>},
                 image_url: 'ridocker.jpg',
                 price: 38.00)
-
 # . . .
 Product.create!(title: 'Build Chatbot Interactions',
                 description:
@@ -57,4 +57,3 @@ Product.create!(title: 'Programming Crystal',
       </p>},
                 image_url: 'crystal.jpg',
                 price: 40.00)
-
