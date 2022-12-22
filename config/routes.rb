48b3bf7c84e142c9ff5497ec38cb60b5d72a66fa
Nahian Rifaat <nrifaat26@gmail.com>
2022-12-22 17:40:12 +0600

    Task K3 I18n Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index' #as: store_index tells rails to create store_index_path and store_index_url accessor and tests to continue working properly
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all #as: store_index tells rails to create store_index_path and store_index_url accessor and tests to continue working properly
+  end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
